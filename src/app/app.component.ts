996a535f69e5016a25c06d2192f0e697eeb102de
thierry.leveque <thierry.leveque@alithya.com>
2023-11-15 16:52:27 -0500

    First working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {SHARED} from './shared'
+import {SelectButtonModule} from 'primeng/selectbutton'
+import {TranslocoService} from '@ngneat/transloco'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SHARED],
+  imports: [CommonModule, RouterOutlet, SHARED, SelectButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  stateOptions: any[] = [{label: 'Français', value: 'fr'}, {label: 'English', value: 'en'}];
+  langValue = 'fr'
+
+  constructor(private translocoService: TranslocoService) {
+  }
+
+  onLangChange() {
+    this.translocoService.setActiveLang(this.langValue)
+  }
 }
